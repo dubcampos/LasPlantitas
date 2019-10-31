731ba2031423bdd28da6e05f36b01eb5c8d32417
jhonvc <rayan_8020@hotmail.es>
2019-10-30 21:08:37 -0500

    Splash

--- app/src/main/java/com/codigo/recplants/MainActivity.java
+++ app/src/main/java/com/codigo/recplants/MainActivity.java
@@ -1,5 +1,7 @@
 package com.codigo.recplants;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this,Splash.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        },3000);
     }
 }
---
+++ app/src/main/java/com/codigo/recplants/Splash.java
@@ -0,0 +1,21 @@
+package com.codigo.recplants;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+
+
+    }
+}
