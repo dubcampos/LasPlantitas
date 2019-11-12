eeef3b352020a993daeb156530d83e17e8f5a47e
jhonvc <rayan_8020@hotmail.es>
2019-11-11 21:44:54 -0500

    Botton enlazado a registro

--- app/src/main/java/com/codigo/recplants/Actividades/LogingActivity.java
+++ app/src/main/java/com/codigo/recplants/Actividades/LogingActivity.java
@@ -6,24 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.codigo.recplants.MainActivity;
 import com.codigo.recplants.R;
 
 public class LogingActivity extends AppCompatActivity {
-    Button btn_iniciar;
-    Button btn_registro;
+
+    Button btng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loging);
-        btn_iniciar=findViewById(R.id.btn_iniciar);
-        btn_registro=findViewById(R.id.btn_registro);
-        btn_registro.setOnClickListener(new View.OnClickListener() {
+        btng=findViewById(R.id.register);
+        btng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LogingActivity.this,RegistrarseActiviity.class);
-                startActivity(intent);
+                Intent t=new Intent(LogingActivity.this,RegistrarseActiviity.class);
+                startActivity(t);
             }
         });
+
     }
 }
--- app/src/main/java/com/codigo/recplants/MainActivity.java
+++ app/src/main/java/com/codigo/recplants/MainActivity.java
@@ -3,31 +3,55 @@ package com.codigo.recplants;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.codigo.recplants.Actividades.BusquedaActivity;
 import com.codigo.recplants.Actividades.CalculadoraActivity;
 
 import com.codigo.recplants.Actividades.CamaraActivity;
 import com.codigo.recplants.Actividades.HistorialActivity;
+import com.codigo.recplants.Actividades.LogingActivity;
 import com.codigo.recplants.Actividades.RespuestaActivity;
 import com.codigo.recplants.Fragmentos.RespuestaFragment;
 
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView BotonNav;
+    Button btn;
+
+    ImageView img;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button4);
+        img=findViewById(R.id.imageView3);
         BotonNav = findViewById(R.id.button_nav1);
         BotonNav.setOnNavigationItemSelectedListener(navListener);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inten=new Intent(MainActivity.this,LogingActivity.class);
+                startActivity(inten);
+
+            }
+        });
+
+
 
     }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -44,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
                 overridePendingTransition(0,0);
             } else if (itemId == R.id.opcion_busqueda) {
-                Intent intent = new Intent(MainActivity.this, BusquedaActivity.class);
+                Intent intent = new Intent(MainActivity.this, LogingActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivityForResult(intent, 0);
                 overridePendingTransition(0,0);
@@ -57,4 +81,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+
 }
