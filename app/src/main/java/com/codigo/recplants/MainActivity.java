3b61f5c6176e9330aecbd4337e4f60978011d269
jhonvc <rayan_8020@hotmail.es>
2019-12-05 21:53:46 -0500

    Agregar menu MainActivity

--- app/src/main/java/com/codigo/recplants/MainActivity.java
+++ app/src/main/java/com/codigo/recplants/MainActivity.java
@@ -7,10 +7,16 @@ import com.codigo.recplants.Actividades.UsuarioLogueadoActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 import com.codigo.recplants.Actividades.BusquedaActivity;
 import com.codigo.recplants.Actividades.CalculadoraActivity;
@@ -19,36 +25,84 @@ import com.codigo.recplants.Actividades.HistorialActivity;
 import com.codigo.recplants.Actividades.LogingActivity;
 import com.codigo.recplants.Actividades.RespuestaActivity;
 
+import java.util.zip.Inflater;
+
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton btn_floating;
     BottomNavigationView BotonNav;
-
+    Toolbar toolbar1;
     ImageView img;
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img=findViewById(R.id.imageView3);
+        img = findViewById(R.id.imageView3);
         BotonNav = findViewById(R.id.button_nav1);
         BotonNav.setOnNavigationItemSelectedListener(navListener);
-        btn_floating=findViewById(R.id.flotante);
+        btn_floating = findViewById(R.id.flotante);
+        toolbar1 = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+
         btn_floating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,RespuestaActivity.class);
+                Intent intent = new Intent(MainActivity.this, RespuestaActivity.class);
                 startActivity(intent);
             }
         });
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.compartir,menu);
+
+        return true;
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.compartimos:
+                try {
+
+
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+                    String aux = "Descarga la app\n";
+                    aux = aux + "https://play.google.com/store/apps/details?id=com.ea.gp.fifamobile";
+                    i.putExtra(Intent.EXTRA_TEXT, aux);
+                    startActivity(i);
+                }catch (Exception e){
+
+                }
+                break;
+
+
+            case R.id.opciones:
+                Toast.makeText(this,"XDXDXDXDXDX",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.prueba:
+                Toast.makeText(this,"la rata",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.observar:
+                Toast.makeText(this,":V",Toast.LENGTH_SHORT).show();
+                break;
+
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
