f9eb713a78d2b866f739cbdfa961a5c9a8ee956d
crisgon123 <crisgon123@gmail.com>
2019-12-12 21:55:15 -0500

    avance post retrofit diagnostico

--- app/src/main/java/com/codigo/recplants/Fragmentos/RespuestaFragment.java
+++ app/src/main/java/com/codigo/recplants/Fragmentos/RespuestaFragment.java
@@ -6,14 +6,25 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.codigo.recplants.Actividades.RespuestaActivity;
+import com.codigo.recplants.Interfaces.Servicios;
 import com.codigo.recplants.MainActivity;
 import com.codigo.recplants.R;
+import com.codigo.recplants.clases.Historialgeneral;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,6 +49,25 @@ public class RespuestaFragment extends Fragment {
         if(muestraCamara) {
             Intent TomarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(TomarFoto, 200);
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://jalexish54.pythonanywhere.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            Servicios service = retrofit.create(Servicios.class);
+            Call<Historialgeneral> historial = service.historialRegistro(1,1);
+            historial.enqueue(new Callback<Historialgeneral>() {
+                                  @Override
+                                  public void onResponse(Call<Historialgeneral> call, Response<Historialgeneral> response) {
+                                      Log.e("nooo", String.valueOf(response.code()));
+                                      Toast.makeText(getContext(),"voy por buen camino",Toast.LENGTH_LONG).show();
+                                  }
+
+                                  @Override
+                                  public void onFailure(Call<Historialgeneral> call, Throwable t) {
+
+                                  }
+                              }
+            );
         }
         return view;
 
--- app/src/main/java/com/codigo/recplants/Interfaces/Servicios.java
+++ app/src/main/java/com/codigo/recplants/Interfaces/Servicios.java
@@ -2,11 +2,15 @@ package com.codigo.recplants.Interfaces;
 
 import com.codigo.recplants.clases.Enfermedad;
 import com.codigo.recplants.clases.Historialgeneral;
+import com.codigo.recplants.clases.Usuario;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface Servicios {
@@ -18,4 +22,13 @@ public interface Servicios {
 
     @GET("API/historial/{id}")
     Call<List<Historialgeneral>> obtenerHistorialID(@Path("id") int id);
+
+
+    @FormUrlEncoded
+    @POST("API/historialregistro/")
+    Call<Historialgeneral>  historialRegistro(
+            @Field("usuario") int usuario,
+            @Field("cultivo") int cultivo
+            //@Field("Imagen_usuarioCultivo") String Imagen_usuarioCultivo
+    );
 }
